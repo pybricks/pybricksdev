d0ac9a5863ee2432d1c1904b92a350e566fc48e0
Laurens Valk <laurens@pybricks.com>
2020-06-01 23:18:46 +0200

    main: connect, send, receive demo

--- develop/main.py
+++ develop/main.py
@@ -1,3 +1,82 @@
-from pybricks_tools import __version__
 
-print(__version__)
+import asyncio
+from bleak import BleakClient, BleakScanner
+
+bleNusCharRXUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+bleNusCharTXUUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+
+
+# Scan for a device with the given name. To do: search by service instead.
+async def scan(device_name, timeout=5):
+
+    # Flag raised by detection of a device
+    device_discovered = False
+
+    def set_device_discovered(*args):
+        nonlocal device_discovered
+        device_discovered = True
+
+    # Create scanner object and register callback to raise discovery flag
+    scanner = BleakScanner()
+    scanner.register_detection_callback(set_device_discovered)
+
+    # Start the scanner
+    await scanner.start()
+
+    INTERVAL = 0.1
+
+    # Sleep until a device of interest is discovered
+    # It would be nice to use a filter, but this hack
+    # is a simple way to keep it cross platform.
+    for i in range(round(timeout/INTERVAL)):
+        # If device_discovered flag is raised, check if it's the right one.
+        if device_discovered:
+            # Unset the flag so we only check if raised again.
+            device_discovered = False
+            # Check if any of the devices found so far has the expected name.
+            devices = await scanner.get_discovered_devices()
+            for dev in devices:
+                # If the name matches, stop scanning and return device.
+                if device_name in dev.name:
+                    await scanner.stop()
+                    return dev
+        # Await until we check again.
+        await asyncio.sleep(INTERVAL)
+
+    # If we are here, scanning has timed out.
+    await scanner.stop()
+
+
+def test_callback(client, *args):
+    print("Disconnected", client.address)
+
+
+def notification_handler(sender, data):
+    print(data)
+
+
+# Main function, to be replaced with an argparser
+async def main():
+    # Scan for a Pybricks Hub
+    device = await scan('Pybricks Hub', timeout=5)
+    if device is not None:
+        print("Found", device.name, "at", device.address)
+    else:
+        raise OSError("Device not found!")
+
+    print("Connecting...")
+
+    # Connect to detected device and start listening for its output
+    async with BleakClient(device.address) as client:
+        client.set_disconnected_callback(test_callback)
+        await client.is_connected()
+        print("Connected!")
+        await client.start_notify(bleNusCharTXUUID, notification_handler)
+        # await asyncio.sleep(2.0)
+        await client.write_gatt_char(bleNusCharRXUUID, b'1234')
+        await asyncio.sleep(2.0)
+        await client.stop_notify(bleNusCharTXUUID)
+        # await client.disconnect()
+
+
+asyncio.run(main())
