d8e54a43180a7a2c9366cb55bdafee17bb14292a
Laurens Valk <laurens@pybricks.com>
2020-06-02 23:04:26 +0200

    demo: make dummy sample bigger

--- demo/hello.py
+++ demo/hello.py
@@ -102,3 +102,303 @@ a = a + 1
 a = a + 1
 a = a + 1
 print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+a = a + 1
+print(a)
