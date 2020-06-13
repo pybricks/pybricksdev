9966c54a1f9e1b71d26b2b657d3edf13819c0ea6
Laurens Valk <laurens@pybricks.com>
2020-06-13 21:04:56 +0200

    connections: document EV3Connection

--- pybricksdev/connections.py
+++ pybricksdev/connections.py
@@ -68,39 +68,56 @@ async def find_ble_device(name, timeout=5):
 
 
 class EV3Connection():
-    """EV3 Pybricks MicroPython SSH wrapper around asyncssh client."""
+    """ev3dev SSH connection for running pybricks-micropython scripts.
+
+    This wraps convenience functions around the asyncssh client.
+    """
 
     _HOME = '/home/robot'
     _USER = 'robot'
     _PASSWORD = 'maker'
 
-    
-
     async def connect(self, address):
-        """Connect to EV3 or get existing connection."""
+        """Connects to ev3dev using SSH with a known IP address.
+
+        Arguments:
+            address (str):
+                IP address of the EV3 brick running ev3dev.
+
+        Raises:
+            OSError:
+                Connect failed.
+        """
 
         print("Connecting to", address, "...", end=" ")
         self.client = await asyncssh.connect(
             address, username=self._USER, password=self._PASSWORD
         )
         print("Connected.", end=" ")
-
         self.client.sftp = await self.client.start_sftp_client()
         await self.client.sftp.chdir(self._HOME)
         print("Opened SFTP.")
 
     async def beep(self):
-        """Runs a command on the shell and returns stdout and stderr."""
+        """Makes the EV3 beep."""
         await self.client.run('beep')
 
     async def disconnect(self):
-        """Close the connection."""
+        """Closes the connection."""
         self.client.sftp.exit()
         self.client.close()
 
-    async def download(self, file_path):
+    async def download(self, local_path):
+        """Downloads a file to the EV3 Brick using sftp.
+
+        Arguments:
+            local_path (str):
+                Path to the file to be downloaded. Relative to current working
+                directory. This same tree will be created on the EV3 if it
+                does not already exist.
+        """
         # Compute paths
-        dirs, file_name = os.path.split(file_path)
+        dirs, file_name = os.path.split(local_path)
 
         # Make sure same directory structure exists on EV3
         if not await self.client.sftp.exists(os.path.join(self._HOME, dirs)):
@@ -112,15 +129,22 @@ class EV3Connection():
                     await self.client.sftp.mkdir(os.path.join(self._HOME, total))
 
         # Send script to EV3
-        remote_path = os.path.join(self._HOME, file_path)
-        await self.client.sftp.put(file_path, remote_path)
+        remote_path = os.path.join(self._HOME, local_path)
+        await self.client.sftp.put(local_path, remote_path)
         return remote_path
 
-    async def pybricks(self, file_path):
-        """Download and run a Pybricks MicroPython script."""
+    async def run(self, local_path):
+        """Downloads and runs a Pybricks MicroPython script.
+
+        Arguments:
+            local_path (str):
+                Path to the file to be downloaded. Relative to current working
+                directory. This same tree will be created on the EV3 if it
+                does not already exist.
+        """
 
         # Send script to the hub
-        remote_path = await self.download(file_path)
+        remote_path = await self.download(local_path)
 
         # Run it and return stderr to get Pybricks MicroPython output
         print("Now starting:", remote_path)
@@ -138,7 +162,15 @@ class EV3Connection():
                 except asyncio.exceptions.TimeoutError:
                     pass
 
-    async def get(self, file_path, local_path=None):
+    async def get(self, remote_path, local_path=None):
+        """Gets a file from the EV3 over sftp.
+
+        Arguments:
+            remote_path (str):
+                Path to the file to be fetched. Relative to ev3 home directory.
+            local_path (str):
+                Path to save the file. Defaults to same as remote_path.
+        """
         if local_path is None:
-            local_path = file_path
-        await self.client.sftp.get(os.path.join(self._HOME, file_path), localpath=local_path)
+            local_path = remote_path
+        await self.client.sftp.get(os.path.join(self._HOME, remote_path), localpath=local_path)
