66882b216a2f46d8371979ba0d477a7fb4bb4c0a
Laurens Valk <laurens@pybricks.com>
2020-06-14 20:21:38 +0200

    ble: generic BLEStreamConnection
    This will form the new basis of the PUPConnection class. It wraps a
    bleak client to conveniently process incoming characters or lines,
    and write data back. Since this is independent of the Pybricks
    protocol to download and run programs, this lives in the ble module.

--- pybricksdev/ble.py
+++ pybricksdev/ble.py
@@ -1,4 +1,4 @@
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 from asyncio import sleep
 
 
@@ -63,3 +63,116 @@ async def find_device(name, timeout=5):
     raise TimeoutError(
         "Could not find {0} in {1} seconds".format(name, timeout)
     )
+
+
+class BLEStreamConnection():
+
+    def __init__(self, char_rx_UUID, char_tx_UUID, mtu, EOL=b'\r\n'):
+        """Initializes and configures connection settings.
+
+        Arguments:
+            char_rx_UUID (str):
+                UUID for RX.
+            char_rx_UUID (str):
+                UUID for TX.
+            mtr (int):
+                Maximum number of bytes per write operation.
+            EOL (bytes):
+                Character sequence that signifies end of line.
+
+        """
+        self.char_rx_UUID = char_rx_UUID
+        self.char_tx_UUID = char_tx_UUID
+        self.EOL = EOL
+        self.mtu = mtu
+        self.char_buf = bytearray(b'')
+
+    def char_handler(self, char):
+        """Handles new incoming characters. Intended to be overridden.
+
+        Arguments:
+            char (int):
+                Character/byte to process
+        """
+        pass
+
+    def line_handler(self, line):
+        """Handles new incoming lines. Intended to be overridden.
+
+        The default just prints the line that comes in.
+
+        Arguments:
+            line (bytearray):
+                Line to process.
+        """
+        print(line)
+
+    def disconnected_handler(self, client, *args):
+        """Handles disconnected event.  Intended to be overridden."""
+        print("Disconnected")
+
+    def _data_handler(self, sender, data):
+        """Handles new incoming data. Calls char and line parsers when needed.
+
+        Arguments:
+            sender (str):
+                Sender uuid.
+            data (bytearray):
+                Incoming data.
+        """
+
+        # Append all new characters to buffer
+        self.char_buf += data
+
+        # For each new character, call its handler
+        for b in data:
+            self.char_handler(b)
+
+        # Break up data into lines and take those out of the buffer
+        lines = []
+        while True:
+            # Find and split at end of line
+            index = self.char_buf.find(self.EOL)
+            # If no more line end is found, we are done
+            if index < 0:
+                break
+            # If we found a line, save it, and take it from the buffer
+            lines.append(self.char_buf[0:index])
+            del self.char_buf[0:index+len(self.EOL)]
+
+        # Call handler for each line that we found
+        for line in lines:
+            self.line_handler(line)
+
+    async def connect(self, address):
+        """Creates connection to server at given address.
+
+        Arguments:
+            address (str):
+                Client address
+        """
+
+        print("Connecting to {0}".format(address))
+        self.client = BleakClient(address)
+        await self.client.connect()
+        self.client.set_disconnected_callback(self.disconnected_handler)
+        await self.client.start_notify(self.char_tx_UUID, self._data_handler)
+        print("Connected successfully!")
+
+    async def disconnect(self):
+        """Disconnects the client from the server."""
+        await self.client.stop_notify(self.char_tx_UUID)
+        await self.client.disconnect()
+
+    async def write(self, data):
+        """Write bytes to the server, split to chunks of maximum mtu size.
+
+        Arguments:
+            data (bytearray):
+                Data to be sent to the server.
+        """
+        chunks = [data[i: i + self.mtu] for i in range(0, len(data), self.mtu)]
+        for i, chunk in enumerate(chunks):
+            print("TX: {0}".format(chunk))
+            await sleep(0.05)
+            await self.client.write_gatt_char(self.char_rx_UUID, chunk)
