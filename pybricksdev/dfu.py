92f31e50360b0bc21f746b95832b6a77a64b9a40
David Lechner <david@pybricks.com>
2020-10-22 11:36:41 -0500

    dfu: add fallback to use dfu-util command line tool
    Installing libusb on Windows and configuring it is a pain. It is much
    easier to get dfu-util. This adds a fallback to use dfu-util if pydfu
    fails.
    
    Recommended solution for now is to download http://dfu-util.sourceforge.net/releases/dfu-util-0.9-win64.zip
    and copy `dfu-util-static.exe` to `.venv\Scripts\` in the project directory.
    
    Also fixed failure due to missing build directory by using temporary directory.
    This way we don't leave files behind on the users computer.

--- pybricksdev/dfu.py
+++ pybricksdev/dfu.py
@@ -4,9 +4,12 @@
 import os
 import sys
 
-from . import _dfu_upload, _dfu_create
+from subprocess import call
+from tempfile import TemporaryDirectory
 
-from .compile import BUILD_DIR
+from usb.core import NoBackendError
+
+from . import _dfu_upload, _dfu_create
 
 ADDRESS = 0x08008000
 VENDOR_ID = 0x0694
@@ -21,24 +24,51 @@ def flash_dfu(firmware_bin, metadata):
         print('Unknown hub type', file=sys.stderr)
         exit(1)
 
-    # Create dfu file
-    outfile = os.path.join(BUILD_DIR, 'firmware.dfu')
-    target = {'address': ADDRESS, 'data': firmware_bin}
-    device = "0x{0:04x}:0x{1:04x}".format(VENDOR_ID, product_id)
-    _dfu_create.build(outfile, [[target]], device)
+    with TemporaryDirectory() as out_dir:
+        # Create dfu file
+        outfile = os.path.join(out_dir, 'firmware.dfu')
+        target = {'address': ADDRESS, 'data': firmware_bin}
+        device = "0x{0:04x}:0x{1:04x}".format(VENDOR_ID, product_id)
+        _dfu_create.build(outfile, [[target]], device)
 
-    # Init dfu tool
-    _dfu_upload.__VID = VENDOR_ID
-    _dfu_upload.__PID = product_id
-    _dfu_upload.init()
-    elements = _dfu_upload.read_dfu_file(outfile)
+        try:
+            # Init dfu tool
+            _dfu_upload.__VID = VENDOR_ID
+            _dfu_upload.__PID = product_id
+            _dfu_upload.init()
+            elements = _dfu_upload.read_dfu_file(outfile)
 
-    # Erase flash
-    print("Erasing flash...")
-    _dfu_upload.mass_erase()
+            # Erase flash
+            print("Erasing flash...")
+            _dfu_upload.mass_erase()
 
-    # Upload dfu file
-    print("Writing new firmware...")
-    _dfu_upload.write_elements(elements, True, _dfu_upload.cli_progress)
-    _dfu_upload.exit_dfu()
-    print("Done.")
+            # Upload dfu file
+            print("Writing new firmware...")
+            _dfu_upload.write_elements(elements, True, _dfu_upload.cli_progress)
+            _dfu_upload.exit_dfu()
+            print("Done.")
+        except NoBackendError:
+            # if libusb was not found, try using dfu-util command line tool
+
+            # prefer dfu-util-static for Windows
+            dfu_util = "dfu-util-static"
+
+            if call([dfu_util, "--version"]):
+                # fall back to dfu-util if dfu-util-static was not found
+                dfu_util = "dfu-util"
+
+            if call([dfu_util, "--version"]):
+                print("No working DFU found.",
+                      "Please install libusb or ensure dfu-util is in PATH.",
+                      file=sys.stderr)
+                exit(1)
+
+            exit(call([
+                dfu_util,
+                "--device",
+                f"{VENDOR_ID}:{product_id}",
+                "--alt",
+                "0",
+                "--download",
+                outfile
+            ]))
