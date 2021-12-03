pybricks/pybricksdev

commit 73d3cdb219639441abab0140259b0e72d924802d

flash: Don't customize SPIKE firmwares.
We will be using the firmware.bin file from the ZIP archive without changes.

pybricksdev/flash.py
@@ -49,9 +49,16 @@ async def create_firmware(
     """
 
     archive = zipfile.ZipFile(firmware_zip)
+    metadata = json.load(archive.open("firmware.metadata.json"))
+
+    # For SPIKE Hubs, we can use the firmware without changes.
+    if metadata["device-id"] in (HubKind.TECHNIC_SMALL, HubKind.TECHNIC_LARGE):
+        firmware = bytearray(archive.open("firmware.bin").read())
+        return firmware, metadata
+
+    # For Powered Up hubs, we append a script and checksum to a base firmware.
     base = archive.open("firmware-base.bin").read()
     main_py = io.TextIOWrapper(archive.open("main.py"))
-    metadata = json.load(archive.open("firmware.metadata.json"))
 
     mpy = await compile_file(
         save_script(main_py.read()),

pybricksdev/cli/__init__.py
@@ -227,7 +227,7 @@ class Flash(Tool):
         print("Creating firmware")
         firmware, metadata = await create_firmware(args.firmware, args.name)
 
-        if metadata["device-id"] == HubKind.TECHNIC_LARGE:
+        if metadata["device-id"] in (HubKind.TECHNIC_SMALL, HubKind.TECHNIC_LARGE):
             from ..dfu import flash_dfu
 
             flash_dfu(firmware, metadata)

commit 97c2c244ae777fd5893dc8e5838312a73c01d26c

flash: Fix name setter for SPIKE.
This was broken by 73d3cdb219639441abab0140259b0e72d924802d.

pybricksdev/flash.py
@@ -26,6 +26,27 @@ from .tools.checksum import crc32_checksum, sum_complement
 logger = logging.getLogger(__name__)
 
 
+def update_hub_name(firmware, metadata, name):
+    """Changes the hub name in the firmware blob."""
+
+    if not name:
+        return
+
+    if semver.compare(metadata["metadata-version"], "1.1.0") < 0:
+        raise ValueError("this firmware image does not support setting the hub name")
+
+    name = name.encode() + b"\0"
+
+    max_size = metadata["max-hub-name-size"]
+    if len(name) > max_size:
+        raise ValueError(
+            f"name is too big - must be < {metadata['max-hub-name-size']} UTF-8 bytes"
+        )
+
+    offset = metadata["hub-name-offset"]
+    firmware[offset : offset + len(name)] = name
+
+
 async def create_firmware(
     firmware_zip: Union[str, os.PathLike, BinaryIO], name: Optional[str] = None
 ) -> Tuple[bytes, dict]:
@@ -51,9 +72,10 @@ async def create_firmware(
     archive = zipfile.ZipFile(firmware_zip)
     metadata = json.load(archive.open("firmware.metadata.json"))
 
-    # For SPIKE Hubs, we can use the firmware without changes.
+    # For SPIKE Hubs, we can use the firmware without appending anything.
     if metadata["device-id"] in (HubKind.TECHNIC_SMALL, HubKind.TECHNIC_LARGE):
         firmware = bytearray(archive.open("firmware.bin").read())
+        update_hub_name(firmware, metadata, name)
         return firmware, metadata
 
     # For Powered Up hubs, we append a script and checksum to a base firmware.
@@ -77,22 +99,8 @@ async def create_firmware(
     # pad with 0s to align to 4-byte boundary
     firmware.extend(0 for _ in range(-len(firmware) % 4))
 
-    if name:
-        if semver.compare(metadata["metadata-version"], "1.1.0") < 0:
-            raise ValueError(
-                "this firmware image does not support setting the hub name"
-            )
-
-        name = name.encode() + b"\0"
-
-        max_size = metadata["max-hub-name-size"]
-        if len(name) > max_size:
-            raise ValueError(
-                f"name is too big - must be < {metadata['max-hub-name-size']} UTF-8 bytes"
-            )
-
-        offset = metadata["hub-name-offset"]
-        firmware[offset : offset + len(name)] = name
+    # Update hub name
+    update_hub_name(firmware, metadata, name)
 
     # append 32-bit little-endian checksum
     if metadata["checksum-type"] == "sum":

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "pybricksdev"
-version = "1.0.0-alpha.17"
+version = "1.0.0-alpha.18"
 description = "Pybricks developer tools"
 authors = ["The Pybricks Authors <dev@pybricks.com>"]
 maintainers = ["Laurens Valk <laurens@pybricks.com>", "David Lechner <david@pybricks.com>" ]