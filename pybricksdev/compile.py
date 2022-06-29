f0342be9c3b10f96a56ec195a8039713b70c6f4c
David Lechner <david@pybricks.com>
2022-06-29 14:01:26 -0500

    compile: make use of mpy_cross_v5 helper
    We can eliminate some code by using the helper function provided by
    the mpy_cross_v5 package. Also add a test for the changed code.

--- pybricksdev/compile.py
+++ pybricksdev/compile.py
@@ -1,11 +1,10 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2019-2020 The Pybricks Authors
+# Copyright (c) 2019-2022 The Pybricks Authors
 
 import asyncio
 import logging
 import os
-from pathlib import Path
-from subprocess import Popen, PIPE
+from typing import List, Optional
 
 import mpy_cross_v5
 
@@ -28,54 +27,15 @@ def make_build_dir():
         raise FileExistsError("A file named build already exists.")
 
 
-async def run_mpy_cross(args):
-    """Runs mpy-cross asynchronously with given arguments.
-
-    Arguments:
-        args:
-            Arguments to pass to mpy-cross.
-
-    Returns:
-        str: stdout.
-
-    Raises:
-        RuntimeError with stderr if mpy-cross fails.
-
-    """
-
-    # Run the process asynchronously
-    try:
-        proc = await asyncio.create_subprocess_exec(
-            mpy_cross_v5.MPY_CROSS_PATH, *args, stdout=PIPE, stderr=PIPE
-        )
-
-        # Check the output for compile errors such as syntax errors
-        stdout, stderr = await proc.communicate()
-    except NotImplementedError:
-        # This error happens when running on Windows with WindowsSelectorEventLoopPolicy()
-        # which is the required policy for ipython kernels due to a requirement
-        # by the tornado package. So in that case, we call the subprocess synchronously
-        # which shouldn't be a big deal for running in notebooks. Python versions
-        # before 3.8 also used WindowsSelectorEventLoopPolicy() by default, but
-        # pybricksdev requires at least Python 3.8, so that shouldn't be a problem.
-        logger.debug("calling mpy-cross synchronously")
-        proc = Popen([mpy_cross_v5.MPY_CROSS_PATH, *args], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = proc.communicate()
-
-    if proc.returncode != 0:
-        raise RuntimeError(stderr.decode())
-
-    # On success, return stdout
-    return stdout.decode()
-
-
-async def compile_file(path, compile_args=["-mno-unicode"], mpy_version=5):
+async def compile_file(
+    path: str, compile_args: Optional[List[str]] = None, mpy_version=5
+):
     """Compiles a Python file with mpy-cross and return as bytes.
 
     Arguments:
-        path (str):
+        path:
             Path to script that is to be compiled.
-        compile_args (dict):
+        compile_args:
             Extra arguments for mpy-cross.
         mpy_version (int):
             Expected mpy ABI version.
@@ -89,25 +49,22 @@ async def compile_file(path, compile_args=["-mno-unicode"], mpy_version=5):
     """
 
     # Get version info
-    out = await run_mpy_cross(["--version"])
-    installed_mpy_version = int(out.strip()[-1])
-    if installed_mpy_version != mpy_version:
-        raise ValueError(
-            "Expected mpy-cross ABI v{0} but v{1} is installed.".format(
-                mpy_version, installed_mpy_version
+    with open(path, "r") as f:
+        if mpy_version == 5:
+            loop = asyncio.get_running_loop()
+
+            proc, mpy = await loop.run_in_executor(
+                None,
+                lambda: mpy_cross_v5.mpy_cross_compile(
+                    path, f.read(), no_unicode=True, extra_args=compile_args
+                ),
             )
-        )
 
-    # Make the build directory
-    make_build_dir()
+            proc.check_returncode()
 
-    # Cross-compile Python file to .mpy and raise errors if any
-    mpy_path = os.path.join(BUILD_DIR, Path(path).stem + ".mpy")
-    await run_mpy_cross([path] + compile_args + ["-o", mpy_path])
-
-    # Read the .mpy file and return as bytes
-    with open(mpy_path, "rb") as mpy:
-        return mpy.read()
+            return mpy
+        else:
+            raise ValueError("mpy_version must be 5")
 
 
 def save_script(py_string):
---
+++ tests/test_compile.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2022 The Pybricks Authors
+
+
+import os
+import struct
+from tempfile import NamedTemporaryFile
+
+import pytest
+from pybricksdev.compile import compile_file
+
+
+@pytest.mark.asyncio
+async def test_compile_file():
+    with NamedTemporaryFile("w", delete=False) as f:
+        try:
+            f.write("print('test')")
+            f.close()
+
+            mpy = await compile_file(f.name)
+
+            magic, abi_ver, flags, int_bits = struct.unpack_from("<BBBB", mpy)
+
+            assert chr(magic) == "M"
+            assert abi_ver == 5
+            assert flags == 0
+            assert int_bits == 31
+        finally:
+            os.unlink(f.name)
